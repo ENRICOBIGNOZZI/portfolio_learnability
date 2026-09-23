e5efbae28d499720a6ed92759c83b45ac75607a6
ENRICOBIGNOZZI <88671442+ENRICOBIGNOZZI@users.noreply.github.com>
2026-09-23 12:59:45 +0200

    Use uncapped portfolios and widen tuning grids

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from train_model import train_model
 
 
 lambda_grid = None
-number_of_lambdas = 60
+number_of_lambdas = 120
 lengthscale_multipliers = [
+    0.125,
+    0.25,
     0.5,
+    0.75,
     1.0,
+    1.5,
     2.0,
+    4.0,
 ]
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("--characteristics", nargs="+", default=["all"])
 parser.add_argument("--kernels", nargs="+", choices=["linear", "gaussian", "ntk", "matern12", "matern32", "matern52"])
 args = parser.parse_args()
 characteristics = "all" if args.characteristics == ["all"] else args.characteristics
-max_gross_exposure = 2.0
+max_gross_exposure = None
 kernel_names = [
     "linear",
     "gaussian",
